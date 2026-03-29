28b120de2975b409b0978629da81aa8cb8c74280
imsf04 <144800805+imsf04@users.noreply.github.com>
2026-03-30 02:50:46 +0800

    Complete latest rustlings exercises

--- exercises/tests/tests1.rs
+++ exercises/tests/tests1.rs
@@ -1,21 +1,17 @@
 // tests1.rs
 //
-// Tests are important to ensure that your code does what you think it should
-// do. Tests can be run on this file with the following command: rustlings run
-// tests1
-//
-// This test has a problem with it -- make the test compile! Make the test pass!
-// Make the test fail!
+// In this exercise, you're using `assert!` to test a very basic boolean
+// function. You should be able to replace `assert!()` with the proper
+// assertion in both places.
 //
 // Execute `rustlings hint tests1` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert() {
-        assert!();
+        assert!(true);
     }
 }
--- exercises/tests/tests2.rs
+++ exercises/tests/tests2.rs
@@ -1,17 +1,17 @@
 // tests2.rs
 //
-// This test has a problem with it -- make the test compile! Make the test pass!
-// Make the test fail!
+// In this exercise, you're using `assert_eq!` to compare values. You should be
+// able to replace `assert_eq!()` with the proper assertion in both places.
 //
 // Execute `rustlings hint tests2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
+
 
 #[cfg(test)]
 mod tests {
     #[test]
     fn you_can_assert_eq() {
-        assert_eq!();
+        assert_eq!(1, 1);
     }
 }
